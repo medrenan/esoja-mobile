9b342a4dad48d11cf7edf4a7afb7b5bddac3fd67
Danvargaa <danielvgh@hotmail.com>
2022-10-03 07:53:34 -0300

    teste

---
+++ frontend/src/data/I18n/PicturePhotos.ts
@@ -0,0 +1,21 @@
+export default {
+  pt: {
+    PicturePhotos: {
+      title: 'Imagem das amostras',
+      subtitle: 'Tire uma foto de todas as plantas usadas nas amostras',
+      imagePlaceholder: 'Adicionar imagem',
+      imageUpdatePictureLabel: 'Alterar imagem',
+      finishButton: 'Finalizar'
+    }
+  },
+  en: {
+    PicturePhotos: {
+      title: 'Picture of the photos',
+      subtitle: 'Take a photo of all the plants used in the samples',
+      imagePlaceholder: 'Add image',
+      imageUpdatePictureLabel: 'Change image',
+      finishButton: 'Finish',
+      menuPrincipal:'Go to Menu'
+    }
+  }
+};
---
+++ frontend/src/screens/CreatePlot/PicturePhotos/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { Alert, ScrollView } from 'react-native';
+import { Button } from '../../../components/Button';
+import { PictureInput } from '../../../components/PictureInput';
+import { StepIndicator } from '../../../components/StepIndicator';
+import Title from '../../../components/Title';
+import { PicturePhotosScreenRouteProps } from '../../../data/routes/app';
+import { useAuth } from '../../../hooks/useAuth';
+import { useSample } from '../../../hooks/useSample';
+import { useUpload } from '../../../hooks/useUpload';
+import { translate } from '../../../data/I18n';
+import {
+  Container,
+  FormContainer,
+  NextStepButton,
+  NoNetworkMessage,
+  PictureContainer
+} from './styles';
+
+//Passo 6 ou 4B
+export const PicturePhotos: React.FC<
+  PicturePhotosScreenRouteProps
+> = ({ navigation }) => {
+  const { isConnected } = useAuth();
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { createSample } = useSample();
+  const { selectImage } = useUpload();
+
+  const handleSelectImage = async () => {
+    const uri = await selectImage();
+    setImage(uri);
+  };
+
+  const handlePicturePhotos = async () => {
+    setLoading(true);
+    try {
+      await createSample(image);
+      navigation.navigate('SampleOne');
+    } catch (err) {
+      setLoading(false);
+      Alert.alert(
+        'Error when registering',
+        'Not possible to register the sample'
+      );
+    }
+  };
+
+  return (
+    <ScrollView>
+      <Container>
+        <Title
+          title={translate('PicturePhotos.title')}
+          subtitle={translate('PicturePhotos.subtitle')}
+        />
+        <StepIndicator step={1} indicator={3} />
+        <FormContainer>
+          {isConnected ? (
+            <>
+              <PictureContainer>
+                <PictureInput
+                  model="RETANGLE"
+                  placeholder="PicturePhotos.imagePlaceholder"
+                  updatePictureLabel="PicturePhotos.imageUpdatePictureLabel"
+                  onPress={handleSelectImage}
+                  uri={image}
+                />
+              </PictureContainer>
+              <NextStepButton>
+                <Button
+                  title="Finalizar"
+                  onPress={handlePicturePhotos}
+                  showLoadingIndicator={loading}
+                />
+              </NextStepButton>
+            </>
+          ) : (
+            <>
+              <NoNetworkMessage>
+                You do not have an internet connection. Save the image and try again later.
+              </NoNetworkMessage>
+              <NextStepButton>
+                <Button
+                  title={translate('PicturePhotos.menuPrincipal')}
+                  onPress={() => navigation.navigate('Home')}
+                  showLoadingIndicator={loading}
+                />
+              </NextStepButton>
+            </>
+          )}
+        </FormContainer>
+      </Container>
+    </ScrollView>
+  );
+};
