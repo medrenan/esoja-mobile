d6cb6c8a54028188760cfc5df4a946ca30030a6f
carolinam <carolinamargiott2i@gmail.com>
2022-10-25 08:23:17 -0300

    mais tentativas de fazer o firebase funcionar

--- frontend/android/app/src/main/java/com/esojamobile/MainApplication.java
+++ frontend/android/app/src/main/java/com/esojamobile/MainApplication.java
@@ -12,13 +12,14 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
-
+import com.facebook.react.shell.MainReactPackage;
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
 
 import com.facebook.react.bridge.JSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -32,11 +33,9 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new ReactNativeFirebaseAppPackage());
-      return packages;
+      return Arrays.asList(
+        new MainReactPackage(),
+        new ReactNativeFirebaseAppPackage());
     }
 
     @Override
--- frontend/src/data/services/api.ts
+++ frontend/src/data/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://172.16.4.91:3333/'
+  baseURL: 'http://172.16.8.137:3333/'
   // baseURL: 'https://api-esoja.herokuapp.com/v1'
 });
 
 export const api2= axios.create({
-  baseURL:'http://172.16.4.91:8080'
+  baseURL:'http://172.16.8.137:8080'
 })
