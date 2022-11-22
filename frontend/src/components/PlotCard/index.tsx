04056047f0c7a339255e9cbf725b85125fa5003f
DanVargaa <danielvgh@hotmail.com>
2022-11-22 10:39:42 -0300

    adicionando botão de edição para o plot

--- frontend/src/components/PlotCard/index.tsx
+++ frontend/src/components/PlotCard/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Avatar } from 'react-native-paper';
+import { View } from 'react-native';
+import { Avatar, Button } from 'react-native-paper';
 import { translate } from '../../data/I18n';
 import { Plot } from '../../data/Model/Plot';
+import { AddButton } from '../../screens/Plots/styles';
 import { defaultImage } from '../../utils/default';
+
 import {
   InformationContainer,
   PlotArea,
   PlotCropYear,
   PlotInformationContainer,
   PlotName,
-  PlotProduction
+  PlotProduction,
+  Icon,
+  ViewInformation
 } from './styles';
 
 interface PlotCardProps {
@@ -19,21 +24,30 @@ interface PlotCardProps {
 
 export const PlotCard: React.FC<PlotCardProps> = ({ plot, onPress }) => {
   return (
-    <PlotInformationContainer onPress={() => onPress(plot.id)}>
-      <Avatar.Image source={{ uri: plot?.photo || defaultImage }} />
-      <InformationContainer>
-        <PlotName>{plot?.description}</PlotName>
-        <PlotCropYear>
-          {translate('plots.PlotCardCropYear')}: {plot?.cropYear}
-        </PlotCropYear>
-        <PlotArea>Area: {plot?.areaTotal} hectares</PlotArea>
-        {plot?.expectedProduction && (
-          <PlotProduction>
-            {translate('plots.PlotCardProdExpectation')}:{' '}
-            {plot.expectedProduction.toFixed(2)} ton/ha
-          </PlotProduction>
-        )}
-      </InformationContainer>
+    <PlotInformationContainer>
+    
+        <Avatar.Image source={{ uri: plot?.photo || defaultImage }} />
+        <InformationContainer>
+          <ViewInformation onPress={() => onPress(plot.id)}>
+              <PlotName>{plot?.description}</PlotName>
+              <PlotCropYear>
+                {translate('plots.PlotCardCropYear')}: {plot?.cropYear}
+              </PlotCropYear>
+              <PlotArea>Area: {plot?.areaTotal} hectares</PlotArea>
+              {plot?.expectedProduction && (
+                <PlotProduction>
+                  {translate('plots.PlotCardProdExpectation')}:{' '}
+                  {plot.expectedProduction.toFixed(2)} ton/ha
+                  
+                </PlotProduction>
+                )}
+            </ViewInformation>
+            <Button>
+              <Icon name="edit" />
+            </Button>
+          </InformationContainer>
+
     </PlotInformationContainer>
+    
   );
 };
--- frontend/src/components/PlotCard/styles.ts
+++ frontend/src/components/PlotCard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
-import { RFHeight, RFWidth } from '../../utils/getResponsiveSizes';
+import { RFHeight, RFWidth, RFFontSize } from '../../utils/getResponsiveSizes';
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 export const PlotInformationContainer = styled.TouchableOpacity`
   flex-flow: row;
@@ -8,11 +10,22 @@ export const PlotInformationContainer = styled.TouchableOpacity`
   background-color: ${({ theme }) => theme.colors.background};
   border-radius: ${RFWidth(6)}px;
   margin-top: ${RFHeight(8)}px;
+  
 `;
 
 export const InformationContainer = styled.View`
+  flexDirection: row;
+  alignItems: center;
+  justify-content: space-between;
   flex: 1;
   padding-left: ${RFWidth(16)}px;
+  width:100%;
+  
+`;
+
+export const ViewInformation = styled.TouchableOpacity`
+  maxWidth:80%;
+  
 `;
 
 export const PlotName = styled.Text`
@@ -30,4 +43,20 @@ export const PlotArea = styled.Text`
 
 export const PlotProduction = styled.Text`
   color: ${({ theme }) => theme.colors.text};
+`;
+export const Icon = styled(Feather)`
+  font-size: ${RFFontSize(35)}px;
+
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const AddButton = styled(RectButton)`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  padding: 10px;
+  margin: 35px;
+  border-radius: 50px;
 `;
